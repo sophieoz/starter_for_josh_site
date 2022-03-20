afca5f4e12f4dc7715f86680fe22782eea097472
sophieoz <sophie.zelina2@gmail.com>
2022-03-20 16:57:55 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="container-fluid text-center"><!--div-->
+                  <img class="img-fluid" src="imgs/landscape.jpg" alt="landscape">
+
+                <div class="container"><!--div-->
+                    <div class="row"><!--row-->
+                        <div class="col-md-6 ">
+                          <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+                        </div>
+                        <div class="col-md-6 ">
+                            <p>Terms and Conditions</p>
+              </div><!--row-->
+                    </div><!--Container-->
+                </div><!--container-fluid-->
+            </footer>
+            <?php get_footer();?>
+</body>
+</html>
