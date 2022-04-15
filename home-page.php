481b736974b3b2e2ffc6c872f1c082fd07b71285
sophieoz <sophie.zelina2@gmail.com>
2022-04-15 16:54:32 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -6,9 +6,11 @@
 <?php get_header();?>   <!--  Tells WordPress to include header.php -->
          <section class= "container-fluid aboutbg text-center">
             <div class="container">
+              
+
             <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
             <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
-<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
+            <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
 
             </div> <!--container-->
         <div class="blocker"></div>        
@@ -16,8 +18,8 @@
         <section class="container-fluid articlesbg">
             <div class="container">
                     <div class="row">
-                    <div class="col-md-4">
                     <h3 CLASS="text-center"><?php the_field('titleforarticles'); ?></h3>
+                    <div class="col-md-4">
                       <img class="img-fluid" src="<?php the_field('articleimage');?>" alt="woman">
                       <h4 class="article-title"><?php the_field('articletitle'); ?> </h4>
                       <p class="date" ><?php the_field('articledate'); ?></p>
@@ -25,20 +27,19 @@
                       <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
                     </div>
                     <div class="col-md-4">
-                    <h3 CLASS="text-center"><?php the_field('titleforarticles'); ?></h3>
-                      <img class="img-fluid" src="<?php the_field('articleimage');?>" alt="model">
-                      <h4 class="article-title"><?php the_field('articletitle'); ?></h4>
-                      < <p class="date" ><?php the_field('articledate'); ?></p>
-                      <p class="article-p"><?php the_field('articleexcerpt_middle'); ?> </p>
-                      <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
+                      <img class="img-fluid" src="<?php the_field('articleimagemiddle');?>" alt="model">
+                      <h4 class="article-title"><?php the_field('articletitlemiddle'); ?></h4>
+                      < <p class="date" ><?php the_field('articledatemiddle'); ?></p>
+                      <p class="article-p"><?php the_field('articleexcerptmiddle'); ?> </p>
+                      <a href="<?php the_field('readmoremiddle'); ?>" class="readmore">CONTINUE READING</a>
                     </div>
                     <div class="col-md-4">
     
-                      <img class="img-fluid" src="<?php the_field('articleimage');?>" alt="map">
-                      <h4 class="article-title"><?php the_field('articletitle'); ?></h4>
-                      < <p class="date" ><?php the_field('articledate'); ?></p>
-                      <p class="article-p"><?php the_field('articleexcerpt_end'); ?> </p>
-                      <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
+                      <img class="img-fluid" src="<?php the_field('articleimagelast');?>" alt="map">
+                      <h4 class="article-title"><?php the_field('articletitlelast'); ?></h4>
+                      < <p class="date" ><?php the_field('articledatelast'); ?></p>
+                      <p class="article-p"><?php the_field('articleexcerptlast'); ?> </p>
+                      <a href="<?php the_field('readmore'); ?>" class="readmorelast">CONTINUE READING</a>
                     </div>
                     </div><!--row-->
                 </div><!--container-->
